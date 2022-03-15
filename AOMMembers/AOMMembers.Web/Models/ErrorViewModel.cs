61f78a17066684da88304b3866f4c0ed8be15ae1
Tsvetelin Ivanov <tzendy01@abv.bg>
2022-03-15 15:47:01 +0200

    Add default AspNetCoreProject

---
+++ AOMMembers/AOMMembers.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AOMMembers.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
